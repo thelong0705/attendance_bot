ff3c85148a884db95db5e18d42402dbe97b80a2b
LongDT <doan.the.long@moneyforward.co.jp>
2020-05-03 18:24:35 +0900

    init

---
+++ lib/attendance_bot.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'attendance_bot/version'
+
+module AttendanceBot
+  class Error < StandardError; end
+  # Your code goes here...
+  class Test
+    def self.hello_world
+      p 'hello world'
+    end
+  end
+end
---
+++ lib/attendance_bot/version.rb
@@ -0,0 +1,3 @@
+module AttendanceBot
+  VERSION = "0.1.0"
+end
