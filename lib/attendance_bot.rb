d7a58c4d4681fa40abe7a3aa76f2ddfaab7e48a5
LongDT <doan.the.long@moneyforward.co.jp>
2020-05-04 23:25:42 +0900

    added login

--- lib/attendance_bot.rb
+++ lib/attendance_bot.rb
@@ -1,13 +1,29 @@
 # frozen_string_literal: true
 
+require 'mechanize'
+require 'rubygems'
+require 'pry'
 require 'attendance_bot/version'
 
 module AttendanceBot
   class Error < StandardError; end
   # Your code goes here...
-  class Test
-    def self.hello_world
-      p 'hello world'
+  class Bot
+    def self.checkin
+      agent = Mechanize.new
+      page = agent.get('https://attendance.moneyforward.com/employee_session/new')
+      login_form = page.form(action: '/employee_session')
+
+      office_field = login_form.fields.find { |f| f.name == 'employee_session_form[office_account_name]' }
+      email_field = login_form.fields.find { |f| f.name == 'employee_session_form[account_name_or_email]' }
+      password_field = login_form.fields.find { |f| f.name == 'employee_session_form[password]' }
+
+      office_field.value = ''
+      email_field.value = ''
+      password_field.value = ''
+
+
+      agent.submit(login_form)
     end
   end
 end
