3d9460bcb6b979207fb4ef8ad3a6465a20bf685b
LongDT <doan.the.long@moneyforward.co.jp>
2020-05-06 17:56:52 +0900

    refactor code

--- lib/attendance_bot.rb
+++ lib/attendance_bot.rb
@@ -8,10 +8,16 @@ require 'config'
 
 module AttendanceBot
   class Error < StandardError; end
-  # Your code goes here...
+
   class Bot
-    def self.checkin
-      agent = Mechanize.new
+    attr_accessor :agent
+
+    def initialize
+      @agent = Mechanize.new
+      Config.load_and_set_settings('./config/config.yml')
+    end
+
+    def login
       page = agent.get('https://attendance.moneyforward.com/employee_session/new')
       login_form = page.form(action: '/employee_session')
 
@@ -19,42 +25,26 @@ module AttendanceBot
       email_field = login_form.fields.find { |f| f.name == 'employee_session_form[account_name_or_email]' }
       password_field = login_form.fields.find { |f| f.name == 'employee_session_form[password]' }
 
-      Config.load_and_set_settings('./config/config.yml')
-
       office_field.value = Settings.office
       email_field.value = Settings.email
       password_field.value = Settings.password
 
+      login_form.submit
+    end
 
-      page = agent.submit(login_form)
-
-
+    def checkin
+      page = login
       clock_in_form = page.forms.first
-      user_time = clock_in_form.fields.find{ |f| f.name == 'web_time_recorder_form[user_time]' }
+      user_time = clock_in_form.fields.find { |f| f.name == 'web_time_recorder_form[user_time]' }
       user_time.value = Time.now.to_s
 
       clock_in_form.submit
     end
 
-    def self.checkout
-      #TODO: refactor code
-      agent = Mechanize.new
-      page = agent.get('https://attendance.moneyforward.com/employee_session/new')
-      login_form = page.form(action: '/employee_session')
-
-      office_field = login_form.fields.find { |f| f.name == 'employee_session_form[office_account_name]' }
-      email_field = login_form.fields.find { |f| f.name == 'employee_session_form[account_name_or_email]' }
-      password_field = login_form.fields.find { |f| f.name == 'employee_session_form[password]' }
-
-      Config.load_and_set_settings('./config/config.yml')
-
-      office_field.value = Settings.office
-      email_field.value = Settings.email
-      password_field.value = Settings.password
-      page = agent.submit(login_form)
-
+    def checkout
+      page = login
       clock_out_form = page.forms[1]
-      user_time = clock_out_form.fields.find{ |f| f.name == 'web_time_recorder_form[user_time]' }
+      user_time = clock_out_form.fields.find { |f| f.name == 'web_time_recorder_form[user_time]' }
       user_time.value = Time.now.to_s
 
       clock_out_form.submit
--- lib/attendance_bot/cli.rb
+++ lib/attendance_bot/cli.rb
@@ -4,9 +4,19 @@ require 'attendance_bot'
 
 module AttendanceBot
   class CLI < Thor
+    def initialize(args = nil, options = nil, config = nil)
+      @bot = AttendanceBot::Bot.new
+      super
+    end
+
     desc 'checkin', 'checkin with current time'
     def checkin
-      AttendanceBot::Bot.checkin
+      @bot.checkin
+    end
+
+    desc 'checkout', 'checkout with current time'
+    def checkout
+      @bot.checkout
     end
   end
 end
